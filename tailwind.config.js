d6e38e6135feacec00ab02e2b85fc84d388c153f
pakfragcom <pakfrag@gmail.com>
2025-07-24 16:26:12 +0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-sans">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
