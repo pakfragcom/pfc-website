978f2f5d6b3b6bba96cbb4ad0e07273e7fb6114b
pakfragcom <pakfrag@gmail.com>
2025-07-24 16:31:14 +0500

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import ScrollToTop from '../components/ScrollToTop'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <Component {...pageProps} />
       <ScrollToTop />
     </>
