03dd1c01e41647a3f62c54f1d49a68bb1138bdb6
pakfragcom <pakfrag@gmail.com>
2025-07-24 13:32:14 +0500

    Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-6 border-t border-gray-800 text-sm text-gray-500 bg-black mt-20">
+      &copy; {new Date().getFullYear()} Pakistan Fragrance Community. All rights reserved.
+    </footer>
+  )
+}
