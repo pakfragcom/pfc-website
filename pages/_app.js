625ec61eecdcba46c9444667856cbc1ce4640807
pakfragcom <pakfrag@gmail.com>
2025-08-04 01:39:11 +0500

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,11 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="icon"
+          href="/pfc-round.png"
+          type="image/png"
+        />
       </Head>
 
       <Component {...pageProps} />
