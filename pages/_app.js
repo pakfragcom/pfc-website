3e3748e9bf219c2bb1744c59c473760883dda8c7
pakfragcom <pakfrag@gmail.com>
2025-08-04 02:58:33 +0500

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,31 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+
+        {/* Favicon */}
         <link
           rel="icon"
           href="/pfc-round.png"
           type="image/png"
+        />
+
+        {/* Structured Data for SEO */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Pakistan Fragrance Community",
+              url: "https://pakfrag.com",
+              logo: "https://pakfrag.com/pfc-round.png",
+            }),
+          }}
         />
       </Head>
 
